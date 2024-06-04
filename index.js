125700ef6aa4dac547bee4ef17bae74ff49aa392
rahidhaque <ontirahid@yahoo.com>
2024-06-04 17:23:44 +0600

    MongoDB Implemented and Post user api created

--- index.js
+++ index.js
@@ -16,6 +16,45 @@ app.use(cors(corsOption));
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iedqjux.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    const userCollection = client.db('hrWorkFlowHubDB').collection('users');
+
+    app.post('/users', async(req,res)=>{
+        const user= req.body;
+        const query = {email: user.email};
+        const existingUser = await userCollection.findOne(query);
+        if(existingUser){
+          return res.send({message: 'User already exists'});
+        }
+        const result= await userCollection.insertOne(user);
+        res.send(result);
+      })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req,res)=>{
     res.send("Website server is running");
 })
