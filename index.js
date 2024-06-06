5545e0d386664fa8d29bb816e2a15a0056fa97f7
rahidhaque <ontirahid@yahoo.com>
2024-06-07 03:26:49 +0600

    Is verified patch data added in employee side api created

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors(corsOption));
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iedqjux.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -68,6 +68,18 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/users/verify/:id", async(req,res) =>{
+      const id = req.params.id;
+      const result = await userCollection.updateOne(
+        { _id: new ObjectId(id) }, 
+        { $set: { isVerified: true } }
+      );
+      if (result.modifiedCount === 1) {
+        return res.status(200).send({ message: 'Verification status updated successfully' });
+      } else {
+        return res.status(404).send({ message: 'Employee not found or verification status not updated' });
+      }
+    })
 
     app.post('/users', async(req,res)=>{
         const user= req.body;
